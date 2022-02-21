b8e6a67230bd7e775c2294ea45d7b6fdfffd4268
Hyun Jong Won <howard.hj.won@gmail.com>
2022-02-21 12:21:23 -0500

    unity project setup

---
+++ Assets/Scripts/BallRoll.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRoll : MonoBehaviour
+{
+    // To Do
+    // The ball takes more force to roll & more difficult to change its direction of travel as it gets bigger.
+    // Due to this behavior, the ball should not be able to grow more than the maximum size.
+
+    public float growthMultiplier = 0.01f;
+    public float shrinkMultiplier = 0.01f;
+    public float posThreshold = 0.1f;
+    public float impulseThreshold = 0.1f;
+    public float raycastDist = 10f;
+    public bool isRolling = false;
+    private Vector3 prevPos;
+    private Vector3 currPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currPos = transform.position;
+        currPos = prevPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the ball has moved, and the ground underneath is snow. If so, grow the ball.
+        currPos = transform.position;
+        RaycastHit hit;
+        raycastDist = gameObject.GetComponent <MeshRenderer> ().bounds.extents.y;
+        Debug.DrawRay (transform.position, Vector3.down * raycastDist, Color.red);
+        if ((currPos - prevPos).magnitude > posThreshold && Physics.Raycast (transform.position, Vector3.down, out hit, raycastDist) && hit.transform.CompareTag ("Snow")) {
+            Debug.Log ("The ball has moved, and the ground is snow!");
+            Grow ((currPos - prevPos).magnitude * growthMultiplier);
+        }
+
+        prevPos = currPos;
+    }
+
+    // Make the ball bigger
+    public void Grow (float amount) {
+        float currScale = gameObject.transform.localScale.x;
+        Vector3 currPos = gameObject.transform.position;
+        currScale += amount;
+        gameObject.transform.localScale = new Vector3 (currScale, currScale, currScale);
+        // gameObject.transform.position = new Vector3 (currPos.x, gameObject.GetComponent <MeshFilter> ().mesh.bounds.extents.y ,currPos.z);
+        Debug.Log ("The ball has grown by: " + amount);
+    }
+
+    // Make the ball smaller
+    public void Shrink (float amount) {
+        float currScale = gameObject.transform.localScale.x;
+        Vector3 currPos = gameObject.transform.position;
+        currScale -= amount;
+        gameObject.transform.localScale = new Vector3 (currScale, currScale, currScale);
+        // gameObject.transform.position = new Vector3 (currPos.x, gameObject.GetComponent <MeshFilter> ().mesh.bounds.extents.y ,currPos.z);
+        Debug.Log ("The ball has been hit. It has shrunk by: " + amount);
+    }
+
+
+    private void OnCollisionEnter(Collision other) {
+        // Shrink ball if it collides with obstacles or other snowballs.
+        if (other.impulse.magnitude > impulseThreshold && (other.transform.CompareTag ("Obstacle") || other.transform.CompareTag ("Snowball"))) {
+            float hitDamage = other.impulse.magnitude * shrinkMultiplier;
+            Shrink (hitDamage);
+        }
+    }
+
+    private void OnCollisionStay(Collision other) {
+        
+    }
+
+    private void OnCollisionExit(Collision other) {
+        
+    }
+}
---
+++ Assets/Scripts/ShootProjectile.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Niantic.ARDK.AR;
+using Niantic.ARDK.AR.Anchors;
+using Niantic.ARDK.AR.Configuration;
+using Niantic.ARDK.Utilities;
+using Niantic.ARDK.AR.ARSessionEventArgs;
+using Niantic.ARDK.Utilities.Logging;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public GameObject _shootObject;
+    public int _projectileForce = 1;
+    public float swipeThreshold = 0.01f;
+    public float swipeForceMultiplier = 0.1f;
+    public bool pickedUp = false;
+    private float pickupTimer;
+    public float timerThreshold = 4.0f;
+    public GameObject currBall;
+
+    public float pickupDistance;
+    public float pickupOffset = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ARSessionFactory.SessionInitialized += SessionInitialized ();
+
+        // if (_arSession.CurrentFrame == null) {
+        //     return;
+        // }
+
+        // Return Update if there's no touch input.
+        if (PlatformAgnosticInput.touchCount == 0) {
+            // Release ball if one is currently picked up.
+            if (pickedUp && currBall) {
+                Release ();
+            }
+            return;
+        }
+        // Debug.Log ("touch registered!");
+        var touch = PlatformAgnosticInput.GetTouch (0);
+
+        // Debug.Log ("current touch phase: " + touch.phase);
+        // Debug.Log ("current tap count: " + touch.tapCount);
+
+        if (pickupTimer > timerThreshold && !pickedUp) {
+            PickUp ();
+        }
+
+        Ray r = Camera.main.ScreenPointToRay (touch.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast (r, out hit) && hit.transform.CompareTag ("Snowball")) {
+
+            // Check if the current object is same as prev touched ball. If so, increment pickup timer.
+            if (currBall == hit.transform.gameObject) {
+                pickupTimer += Time.deltaTime;
+                Debug.Log ("Current pickup timer: " + pickupTimer);
+            }
+            Debug.Log ("touched a snowball!");
+
+            currBall = hit.transform.gameObject;
+            Debug.Log ("Touch delta position is: " + touch.deltaPosition);
+
+            // Apply force to ball from swipe.
+            if (touch.deltaPosition.magnitude > swipeThreshold) {
+                pickupTimer = 0;
+
+                Vector2 swipeDirection = touch.deltaPosition;
+                Vector3 ZVector = new Vector3 (Camera.main.transform.forward.x, 0, Camera.main.transform.forward.z).normalized * swipeDirection.y;
+                Vector3 YVector = new Vector3 (Camera.main.transform.right.x, 0, Camera.main.transform.right.z).normalized * swipeDirection.x;
+                Vector3 ballForce = (ZVector + YVector) * swipeForceMultiplier;
+
+                currBall.GetComponent <Rigidbody> ().AddForceAtPosition (ballForce, hit.point);
+                Debug.Log ("Player is currently swiping! Applying force to ball");
+            }
+        }
+
+        // if a ball has been currently picked up, then move it to a position offset from the ground / another ball.
+        if (pickedUp) {
+            if (Physics.Raycast (r, out hit) && (hit.transform.CompareTag ("Snow") || hit.transform.CompareTag ("Snowball")) && Vector3.Dot (Vector3.up, hit.normal) > 0.8f) {
+                currBall.transform.position = new Vector3 (hit.point.x, hit.point.y + currBall.GetComponent <MeshRenderer> ().bounds.size.y, hit.point.z);
+                Debug.Log ("Moving the ball to: " + currBall.transform.position);
+            }
+        }    
+    }
+
+    public void PickUp () {
+        pickedUp = true;
+        if (currBall) {
+            currBall.GetComponent <MeshRenderer> ().material.color = Color.red;
+            currBall.GetComponent <Rigidbody> ().isKinematic = true;
+            Debug.Log ("The ball has been picked up!");
+        }
+    }
+
+    public void Release () {
+        pickedUp = false;
+        pickupTimer = 0;
+        if (currBall) {
+            currBall.GetComponent <MeshRenderer> ().material.color = Color.white;
+            currBall.GetComponent <Rigidbody> ().isKinematic = false;
+            currBall = null;
+            Debug.Log ("The ball has been released!");
+        }
+    }
+
+
+
+    public void Shoot () {
+        if (_shootObject) {
+            GameObject shootObject = Instantiate (_shootObject, Camera.main.transform.position, Camera.main.transform.rotation);
+            shootObject.GetComponent <Rigidbody> ().AddForce (Camera.main.transform.forward * _projectileForce);
+            Debug.Log ("Spawned a snowball!");
+        }
+    }
+}
